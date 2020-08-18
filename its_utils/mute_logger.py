cc162c31ff89b4953954f1e4d95f781825d83b9b
hlopik <evg@it-solution.ru>
2020-08-18 18:53:46 +0300

    app_gitpull

---
+++ its_utils/mute_logger.py
@@ -0,0 +1,12 @@
+from its_utils.app_logger.its_logger import ItsLogger
+
+
+class MuteLogger:
+    # Немой логгер
+    # можно использовать при передаче логгера в Битрикс токен вызовы
+    # пример в def call_api_method(
+
+    def do_nothing(self, log_type, message=None, app=None, exc_info=False, params=None):
+        return None
+
+    debug = info = warning = warn = error = exception = critical = do_nothing
