352194f9956ec50cfa57192ba594e4fcdafbde18
hlopik <evg@it-solution.ru>
2021-07-09 16:38:46 +0300

    pysendpulse.py

--- itsolution/functions/cron_starter.py
+++ itsolution/functions/cron_starter.py
@@ -6,6 +6,10 @@ FILE_PATH = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(FILE_PATH, '../../../'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
+# редактируем cron
+# sh crontab -e
+# Раз в минуту будет запускать функцияю test_print
+# * * * * *       /home/app/env_app/bin/python /home/app/app/its_utils/app_cron/cron.py integration_utils.itsolution.functions.cron_starter.test_print
 
 import django
 django.setup()
