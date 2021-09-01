2ffb56c5464e18cf1c040e195e4bfab054fd3b3f
kde <kde@it-solution.ru>
2021-09-01 13:21:49 +0300

    list method wrappers update

--- bitrix24/functions/call_list_method.py
+++ bitrix24/functions/call_list_method.py
@@ -26,6 +26,9 @@ METHOD_WRAPPERS = {
     'tasks.task.history.list': 'list',
     'tasks.task.getFields': 'fields',
     'tasks.task.getaccess': 'allowedActions',
+    'sale.order.list': 'orders',
+    'sale.propertyvalue.list': 'propertyValues',
+    'sale.basketItem.list': 'basketItems',
 }
 
 
