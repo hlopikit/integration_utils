8433a88199cfea2fbfdf79d8f40c936080171fa5
hlopik <evg@it-solution.ru>
2022-01-31 20:41:22 +0300

    a

---
+++ itsolution/functions/log_to_telegram.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from telegram.bot import Bot
+
+def log_to_telegram(text):
+    """
+    исползьзует настройки из settings.py
+    TELEGRAM_LOG_BOT_TOKEN = '20933333:AAHUF233232323AA_DmGewfw332r32r0UI9qk'
+    TELEGRAM_LOG_CHAT_ID = -38283832832832
+
+    :param text:
+    :return:
+    """
+
+    try:
+        Bot(token=settings.TELEGRAM_LOG_BOT_TOKEN).send_message(settings.TELEGRAM_LOG_CHAT_ID, text)
+        return True
+    except Exception:
+        return False
+
+
+
