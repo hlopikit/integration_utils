588ee6ead51484e375fd8440d74ab7cd16e66a3f
hlopik <evg@it-solution.ru>
2022-01-31 12:20:02 +0300

    fast

--- its_utils/app_settings/functions.py
+++ its_utils/app_settings/functions.py
@@ -1,18 +1,21 @@
-def set_value(key, value):
+def set_value(key, value, comment=''):
 
     from settings import ilogger
     from integration_utils.its_utils.app_settings.models import KeyValue
 
     result = KeyValue.objects.filter(key=key).update(value=value)
     if not result:
-        KeyValue.objects.create(key=key, value=value)
+        KeyValue.objects.create(key=key, value=value, comment=comment)
     ilogger.debug('set_value', "{}=>{}".format(key, value))
     return
 
-def get_value(key):
+def get_value(key, create=False, default='', comment=''):
     from integration_utils.its_utils.app_settings.models import KeyValue
 
     try:
         return KeyValue.objects.get(key=key).value
     except KeyValue.DoesNotExist:
+        if create:
+            set_value(key=key, value=default, comment=comment)
+            return default
         return None
