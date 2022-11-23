e9f5b911290dcdd5b12ef7007eda51fc060198a2
kde <kde@it-solution.ru>
2022-11-24 00:27:18 +0600

    robots

--- bitrix_robots/example_robot/urls.py
+++ bitrix_robots/example_robot/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .models import ExampleRobot
-from .views import install
+from .views import install, uninstall
 
 app_name = 'bitrix_robot_example'
 
 urlpatterns = [
-    path('register/', install, name='register'),
+    path('install/', install, name='install'),
+    path('uninstall/', uninstall, name='uninstall'),
     path('handler/', ExampleRobot.as_view(), name='handler'),
 ]
