a1f64f78a0434d80e03c5ebf0f670d96ddf9e6c2
kde <kde@it-solution.ru>
2022-12-22 02:54:24 +0600

    robots base

--- bitrix_robots/errors.py
+++ bitrix_robots/errors.py
@@ -2,9 +2,12 @@ from django.http import HttpResponse
 
 
 class RobotException(Exception):
-    def __init__(self, message, http_status=400):
-        self.message = message
+    def __init__(self, message=None, http_status=400):
+        self.message = type(self).__name__ if message is None else message
         self.http_status = http_status
+
+    def __str__(self):
+        return self.message
 
     def http_response(self):
         return HttpResponse(self.message, status=self.http_status)
@@ -13,3 +16,7 @@ class RobotException(Exception):
 class VerificationError(RobotException):
     def __init__(self, message='Verification failed'):
         super().__init__(message, 401)
+
+
+class DelayProcess(RobotException):
+    pass
