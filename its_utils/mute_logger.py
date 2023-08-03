e8dc4767426d14da33a245557dba9e463972835c
hlopik <evg@it-solution.ru>
2023-08-03 20:47:12 +0300

    Merge remote-tracking branch 'origin/t58831_telegram_bot'
    # Conflicts:
    #	usermanager/views/search_manager.py
    #	usermanager/views/utils.py

--- its_utils/mute_logger.py
+++ its_utils/mute_logger.py
@@ -1,10 +1,15 @@
 
+# Добавить в settings.py
+# from integration_utils.its_utils.mute_logger import MuteLogger
+# ilogger = MuteLogger()
+
+
 class MuteLogger:
     # Немой логгер
     # можно использовать при передаче логгера в Битрикс токен вызовы
     # пример в def call_api_method(
 
-    def do_nothing(self, log_type, message=None, app=None, exc_info=False, params=None):
+    def do_nothing(self, log_type, message=None, app=None, exc_info=False, params=None, **kwargs):
         return None
 
     debug = info = warning = warn = error = exception = critical = do_nothing
