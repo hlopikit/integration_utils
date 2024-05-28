2dc8fc6b18e48d1d4acd35a50fe0d6813589f636
evg <evg@it-solution.ru>
2024-05-28 14:11:37 +0300

    bound_client_box.py

---
+++ bitrix24/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class Config(AppConfig):
+    name = 'integration_utils.bitrix24'
+    #verbose_name = "bitrix_telegram_log для порталов"
+    default_auto_field = "django.db.models.AutoField"
