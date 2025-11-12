bc165dfdcee5b37dd7b6674d6638622416edac16
iiv <iiv@it-solution.ru>
2025-11-12 13:23:17 +0300

    add a new import

--- its_utils/app_datetime/calendar_work_days.py
+++ its_utils/app_datetime/calendar_work_days.py
@@ -1,6 +1,5 @@
 from integration_utils.iu_datetime.calendar_work_days import *
 from settings import ilogger
-
 ilogger.warning(
     'deprecated',
     'integration_utils.its_utils.app_datetime.calendar_work_days is deprecated. '
