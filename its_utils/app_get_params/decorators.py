ebe4459889bfbae4cecb02ae4ddc28b8699c4d4d
evg <evg@it-solution.ru>
2025-11-17 12:17:27 +0300

    from integration_utils.iu_get_params.get_params_from_sources import get_params_from_sources

--- its_utils/app_get_params/decorators.py
+++ its_utils/app_get_params/decorators.py
@@ -1,7 +1,7 @@
 from integration_utils.iu_get_params.decorators import *
-from settings import ilogger
-ilogger.warning(
-    'deprecated',
-    'integration_utils.its_utils.app_get_params is deprecated. '
-    'Use integration_utils.iu_get_params instead.'
-)
+# from settings import ilogger
+# ilogger.warning(
+#     'deprecated',
+#     'integration_utils.its_utils.app_get_params is deprecated. '
+#     'Use integration_utils.iu_get_params instead.'
+# )
