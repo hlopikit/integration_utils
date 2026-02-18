95ea6ed22a5ec55359a291ba8b8b40f91bb179cf
sia <sia@it-solution.ru>
2026-02-18 18:55:39 +0300

    #274935 is_not_logic_error: add is_cant_refresh

--- bitrix24/exceptions_filter_v1.py
+++ bitrix24/exceptions_filter_v1.py
@@ -45,6 +45,7 @@ def is_not_logic_error(exception: BitrixApiException):
             exception.is_sphinx_connect_error,
             exception.is_error_core,
             exception.is_connection_error,
+            exception.is_cant_refresh,
         ]
         if any(error_conditions):
             return True
